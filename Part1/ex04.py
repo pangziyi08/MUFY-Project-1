79a96a50f3f4be25dea923e531491a41f488b000
pangziyi08 <pangziyi2008@gmail.com>
2026-05-26 02:21:47 +0000

    MUFY part 1

---
+++ Part1/ex04.py
@@ -0,0 +1,17 @@
+# num1 and num2 are integers
+num1 = 100
+num2 = 2
+
+# num3 and num4 are strings
+num3 = "50"
+num4 = "5"
+
+# TODO: Fix the data type issue below
+# Convert string variables to the correct type so the math works
+# Insert your code here
+
+# This will work fine
+print(int(num1) / int(num2))
+
+# This will cause an error — do NOT change this line!
+print(int(num3) / int(num4))
